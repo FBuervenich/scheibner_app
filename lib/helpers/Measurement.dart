632e63384c85f8c4cfebcfc2222a432b32b8a2c2
DNeuroth <49400982+DNeuroth@users.noreply.github.com>
2019-05-13 12:58:43 +0200

    ADD measurement class
    Measurement class that contains all measurement values

---
+++ lib/helpers/Measurement.dart
@@ -0,0 +1,7 @@
+class Measurement {
+  double frontwheel = 0;
+  double rearwheel = 0;
+  double frontfork = 0;
+  double rearheight = 0;
+  double swingarmlength = 0;
+}
--- lib/helpers/appmodel.dart
+++ lib/helpers/appmodel.dart
@@ -1,14 +1,51 @@
+import 'package:scheibner_app/helpers/Measurement.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class AppModel extends Model {
-  double _frontwheel = 42;
+  Measurement _meas = new Measurement();
 
   double getFrontwheel() {
-    return _frontwheel;
+    return _meas.frontwheel;
   }
   
   void setFrontwheel(double d) {
-    _frontwheel = d;
+    _meas.frontwheel = d;
+    notifyListeners();
+  }
+
+  double getRearwheel() {
+    return _meas.rearwheel;
+  }
+
+  void setRearwheel(double d) {
+    _meas.rearwheel = d;
+    notifyListeners();
+  }
+
+  double getFrontfork() {
+    return _meas.frontfork;
+  }
+
+  void setFrontfork(double d) {
+    _meas.frontfork = d;
+    notifyListeners();
+  }
+
+  double getRearheight() {
+    return _meas.rearheight;
+  }
+
+  void setRearheight(double d) {
+    _meas.rearheight = d;
+    notifyListeners();
+  }
+
+  double getSwingarmlength() {
+    return _meas.swingarmlength;
+  }
+
+  void setSwingarmlength(double d) {
+    _meas.swingarmlength = d;
     notifyListeners();
   }
 }
