33e8055400a23fe632f786a232441de3ba629298
Kops28 <b.kops@outlook.de>
2019-05-13 14:55:58 +0200

    Fehlerhafte TestAnsicht rausgeworfen

--- lib/data/appmodel.dart
+++ lib/data/appmodel.dart
@@ -1,51 +1,6 @@
-import 'package:scheibner_app/helpers/Measurement.dart';
+import 'package:scheibner_app/data/data.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class AppModel extends Model {
-  Measurement _meas = new Measurement();
-
-  double getFrontwheel() {
-    return _meas.frontwheel;
-  }
-  
-  void setFrontwheel(double d) {
-    _meas.frontwheel = d;
-    notifyListeners();
-  }
-
-  double getRearwheel() {
-    return _meas.rearwheel;
-  }
-
-  void setRearwheel(double d) {
-    _meas.rearwheel = d;
-    notifyListeners();
-  }
-
-  double getFrontfork() {
-    return _meas.frontfork;
-  }
-
-  void setFrontfork(double d) {
-    _meas.frontfork = d;
-    notifyListeners();
-  }
-
-  double getRearheight() {
-    return _meas.rearheight;
-  }
-
-  void setRearheight(double d) {
-    _meas.rearheight = d;
-    notifyListeners();
-  }
-
-  double getSwingarmlength() {
-    return _meas.swingarmlength;
-  }
-
-  void setSwingarmlength(double d) {
-    _meas.swingarmlength = d;
-    notifyListeners();
-  }
+  Data _meas = new Data();
 }
--- lib/pages/simulationPage.dart
+++ lib/pages/simulationPage.dart
@@ -6,23 +6,10 @@ class SimulationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: Center(
+      child: Center(
           //TODO add ergebnissePage
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ScopedModelDescendant<AppModel>(
-                builder: (context, child, data) =>
-                  Text('Data: ${data.getFrontwheel()}')),
-              RaisedButton(
-                onPressed: () {
-                  AppModel model = ScopedModel.of<AppModel>(context);
-                  model.setFrontwheel(model.getFrontwheel() + 1);
-                }
-              )
-            ]
-          )
-        ),
+              mainAxisAlignment: MainAxisAlignment.center, children: [])),
     );
   }
 }
