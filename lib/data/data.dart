3343c47d52941a1f690d88a3b438ea3371d2f861
Kops28 <b.kops@outlook.de>
2019-05-13 14:49:19 +0200

    data obj

---
+++ lib/data/data.dart
@@ -0,0 +1,38 @@
+class Data {
+  //Stammdata
+  double radumfang_vorn = 1889.0;
+  double radumfang_hinten = 1948.0;
+  double abstand_vorn = 886.0;
+  double vorderachshoehe = 60.0;
+  double abstand_hinten = 530.0;
+  double hinterachshoehe = 171.0;
+  double heckhoehe = 550.0;
+  double offset = 33.827778;
+
+  double g_st = -0.100999996; // Sturz
+  double g_rw = 86.46109772; // Rahmenwinkel
+  double g_rl = 732.3649902; // Rahmenlaenge
+  double cmd_winkel = 69; // CMS-Winkel
+  double g_Lv = 0.0; // Lenkkopfver.
+
+  double c1 = 234.0;
+  double c2 = 0.0;
+  double c3 = 0.0;
+  double c4 = 0.0;
+  double c5 = 0.0;
+  double c6 = 0.0;
+  double c7 = 0.0;
+  double c8 = 0.0;
+
+  //X-Data
+  double lkw_grad;
+  double nachlauf_fb;
+  double nachlauf_real;
+  double schwingenwinkel_grad;
+  double gabellaenge;
+  double schwingenlaenge;
+  double radstand;
+
+  double vorderbauhoehe;
+  double schwingenachshoehe;
+}
--- lib/helpers/Measurement.dart
+++
@@ -1,7 +0,0 @@
-class Measurement {
-  double frontwheel = 0;
-  double rearwheel = 0;
-  double frontfork = 0;
-  double rearheight = 0;
-  double swingarmlength = 0;
-}
