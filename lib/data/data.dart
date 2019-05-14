c6bab57050f423c0b7527d38efb85fae374d8e5c
DNeuroth <49400982+DNeuroth@users.noreply.github.com>
2019-05-14 10:11:15 +0200

    CLone-Method for Data
    Clone Method to create copies of a Data object

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -36,4 +36,39 @@ class Data {
   //ResultData
   double vorderbauhoehe;
   double schwingenachshoehe;
+
+  Data() {}
+
+  Data.clone(Data data) {
+    radumfangVorn = data.radumfangVorn;
+    radumfangHinten = data.radumfangHinten;
+    abstandVorn = data.abstandVorn;
+    vorderachshoehe = data.vorderachshoehe;
+    abstandHinten = data.abstandHinten;
+    hinterachshoehe = data.hinterachshoehe;
+    heckhoehe = data.heckhoehe;
+    offset = data.offset;
+    gSt = data.gSt;
+    gRw = data.gRw;
+    gRl = data.gRl;
+    cmdWinkel = data.cmdWinkel;
+    gLv = data.gLv;
+    c1 = data.c1;
+    c2 = data.c2;
+    c3 = data.c3;
+    c4 = data.c4;
+    c5 = data.c5;
+    c6 = data.c6;
+    c7 = data.c7;
+    c8 = data.c8;
+    lkwGrad = data.lkwGrad;
+    nachlauf = data.nachlauf;
+    nachlaufReal = data.nachlaufReal;
+    schwingenwinkelGrad = data.schwingenwinkelGrad;
+    gabellaenge = data.gabellaenge;
+    schwingenlaenge = data.schwingenlaenge;
+    radstand = data.radstand;
+    vorderbauhoehe = data.vorderbauhoehe;
+    schwingenachshoehe = data.schwingenachshoehe;
+  }
 }
