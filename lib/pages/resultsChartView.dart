cf6a580e52344fec2a965b277efae3a650144012
bk3156s <b.kops@outlook.de>
2019-05-16 22:24:56 +0200

    resultsChartView init

--- lib/pages/resultsChartView.dart
+++ lib/pages/resultsChartView.dart
@@ -3,8 +3,42 @@ import 'package:flutter/widgets.dart';
 class ChartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("Write comments and save simulation data"),
+    var size = MediaQuery.of(context).size;
+    /*24 is for notification bar on Android*/
+    final double itemHeight = (size.height - 100 - 24) / 2;
+    final double itemWidth = size.width / 2;
+
+    return Container(
+      child: Center(
+        child: ListView(
+          padding: const EdgeInsets.all(8.0),
+          children: <Widget>[
+            GridView.count(
+              primary: false,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8.0),
+              childAspectRatio: (itemWidth / itemHeight),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: <Widget>[
+                const Text('Chart 1'),
+                const Text('Chart 2'),
+              ],
+            ),
+            GridView.count(
+              primary: false,
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(8.0),
+              childAspectRatio: (size.width / (itemHeight / 2)),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 1,
+              children: <Widget>[
+                const Text('Chart 3'),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/pages/resultsSaveView.dart
+++ lib/pages/resultsSaveView.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/widgets.dart';
 
 class SaveView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(child: new Text("Show results as charts"),);
+    return new Center(
+      child: new Text("Write comments and save simulation data"),
+    );
   }
-
 }
